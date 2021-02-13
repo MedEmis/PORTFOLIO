cf0cc514115b6b2f26e6a9f142fb2db969e97cff
Maksym <draganovmaksim9@gmail.com>
2021-02-13 18:33:18 +0200

    download button added

--- vue.config.js
+++ vue.config.js
@@ -7,3 +7,23 @@ module.exports = {
 		}
 	}
 }
+
+module.exports = {
+	configureWebpack: {
+		module: {
+			rules: [
+				{
+					test: /\.(pdf)(\?.*)?$/,
+					use: [
+						{
+							loader: 'url-loader',
+							options: {
+								name: 'files/[name].[hash:8].[ext]'
+							}
+						}
+					]
+				}
+			]
+		}
+	}
+}
