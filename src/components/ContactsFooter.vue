5e3e1c093a94e18e71fa221fd55c202c784ae417
Maksym <draganovmaksim9@gmail.com>
2021-02-13 10:13:43 +0200

    portfolio v2

---
+++ src/components/ContactsFooter.vue
@@ -0,0 +1,164 @@
+<template>
+  <footer>
+    <b-row class="p-4">
+      <b-col class="px-4" xl="6" lg="6" md="12" sm="12" xs="12">
+        <form
+          action="https://formspree.io/draganovmaksim9@gmail.com"
+          method="post"
+          id="scrolltarget"
+          class="conacts-form"
+        >
+          <fieldset>
+            <legend class="legend">
+              You can send message to me via this form
+            </legend>
+            <b-form-group
+              id="input-group-1"
+              label="Email address:"
+              label-for="input-1"
+            >
+              <b-form-input
+                id="input-1"
+                type="email"
+                placeholder="Enter email"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="input-group-2"
+              label="Your Message:"
+              label-for="input-2"
+            >
+              <b-form-textarea
+                id="input-2"
+                placeholder="...message"
+                required
+              ></b-form-textarea>
+            </b-form-group>
+
+            <b-button block type="submit" variant="primary">Submit</b-button>
+          </fieldset>
+        </form>
+      </b-col>
+      <b-col class="contacts mt-2" xl="6" lg="6" md="12" sm="12" xs="12">
+        <div v-for="item in myContacts" :key="item.name" class="contacts__item">
+          <h4 class="contacts__title">{{ item.name }}</h4>
+          <a target="_blank" :href="item.href" class="contacts__link">{{
+            item.value
+          }}</a>
+          <b-icon
+            class="contacts__icon"
+            :icon="item.icon"
+            scale="2"
+            variant="primary"
+          />
+        </div>
+        <a href="#" class="return-up">
+          <b-icon icon="arrow-up" scale="2.5" variant="primary"
+        /></a>
+      </b-col>
+    </b-row>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "ContactsFooter",
+  data() {
+    return {
+      myContacts: [
+        {
+          name: "My phone",
+          href: "tel:+380681397549",
+          value: "+38 068 139-75-49",
+          icon: "telephone",
+        },
+        {
+          name: "My mail",
+          href: "mailto:draganovmaksim9@gmail.com",
+          value: "draganovmaksim9@gmail.com",
+          icon: "envelope",
+        },
+        {
+          name: "Facebook",
+          href: "https://www.facebook.com/profile.php?id=100025795330285",
+          value: "https://www.facebook.com",
+          icon: "facebook",
+        },
+        {
+          name: "Git Hub",
+          href: "https://github.com/MedEmis",
+          value: "https://github.com/MedEmis",
+          icon: "github",
+        },
+        {
+          name: "Linkedin",
+          href: "https://www.linkedin.com/in/maksym-drahanov-41588b1a9/",
+          value: "https://www.linkedin.com",
+          icon: "linkedin",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+footer {
+  background: rgba(177, 117, 117, 0.329);
+  margin: 10px 0;
+  border-radius: 10px;
+}
+.conacts-form {
+}
+.contacts {
+  padding: 1vh 4vw;
+  &__item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    background: rgba(138, 134, 134, 0.315);
+    margin-bottom: 10px;
+    padding: 5px 5px;
+    border-radius: 5px;
+    padding-right: 35px;
+    @media screen and (max-width: 770px) {
+      display: block;
+    }
+  }
+
+  &__title {
+    font-weight: 700;
+  }
+  &__icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform: translate(-50%, 50%);
+  }
+  &__link {
+    font-size: 18px;
+    color: #fff;
+    @media screen and (max-width: 480px) {
+      margin-right: 20px;
+    }
+    @media screen and (max-width: 350px) {
+      font-size: 16px;
+    }
+  }
+  .return-up {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    background: rgb(112, 112, 112);
+    height: 50px;
+    width: 50px;
+    text-align: center;
+    border-radius: 50%;
+    line-height: 50px;
+    transform: translate(20%, 50%);
+  }
+}
+</style>
---
+++ src/components/HeroHeader.vue
@@ -0,0 +1,217 @@
+<template>
+  <header>
+    <b-row class="hero-header px-2">
+      <b-col class="hero-header__info" xl="8" lg="8" md="12" sm="12" xs="12">
+        <div class="hero-header__info_position">Junior Frontend Developer</div>
+        <hr />
+        <div class="hero-header__info_name">Maksym Drahanov</div>
+        <hr />
+        <div class="hero-header__info_stack">
+          <h3 class="hero-header__info_stack__title">
+            Used tchnologies stack:
+          </h3>
+          <div>
+            <ul class="hero-header__info_stack__list">
+              <li v-for="item in stack" :key="item.name">
+                <b-img
+                  fluid
+                  :src="require(`@/assets/${item.image}`)"
+                  alt="avatar"
+                />
+                <span>{{ item.name }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </b-col>
+      <b-col
+        class="hero-header__avatar p-5"
+        xl="4"
+        lg="4"
+        md="10"
+        sm="10"
+        xs="10"
+      >
+        <b-img
+          fluid
+          class="hero-header__avatar_image"
+          :src="require('@/assets/avatar.jpg')"
+          alt="avatar"
+        ></b-img>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col class="hero-header__about m-4">
+        <h2>About me</h2>
+        <article>
+          <p>I'm a self-taught front-end developer.</p>
+          <p>
+            I want to start my carrer in Software Development as Junion
+            Front-end-Developer.<br />
+            I would like to get real experience in a real company with real
+            projects.
+          </p>
+          <p>
+            During my studying and practising I've got experience with HTML,
+            CSS, JS, React, Redux, Vue, Vuex. <br />
+            Also I have some experience with Wordpress as Headless CMS, which
+            was been used as database.
+          </p>
+        </article>
+      </b-col>
+    </b-row>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeroHeader",
+  data() {
+    return {
+      stack: [
+        {
+          name: "HTML",
+          image: "html.png",
+        },
+        {
+          name: "CSS",
+          image: "css.png",
+        },
+        {
+          name: "SCSS",
+          image: "scss.png",
+        },
+        {
+          name: "JS",
+          image: "js.png",
+        },
+        {
+          name: "React",
+          image: "react.png",
+        },
+        {
+          name: "Redux",
+          image: "redux.png",
+        },
+        {
+          name: "Vue",
+          image: "vue.png",
+        },
+        {
+          name: "Vuex",
+          image: "vuex.png",
+        },
+        {
+          name: "Bootstrap",
+          image: "bootstrap.png",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+header {
+  background: rgb(102, 134, 241);
+  position: relative;
+  margin: 10px 0;
+  border-radius: 10px;
+}
+.hero-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__info {
+    font-weight: 700;
+    font-size: 34px;
+    padding: 3%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    text-align: left;
+    hr {
+      width: 100%;
+      height: 5px;
+      background: rgb(119, 119, 119);
+      border-radius: 5px;
+    }
+    @media screen and (max-width: 769px) {
+      padding: 7%;
+    }
+
+    &_position {
+      color: rgb(204, 142, 9);
+      margin-bottom: 10px;
+    }
+
+    &_name {
+      color: rgb(4, 28, 94);
+      margin-bottom: 10px;
+    }
+
+    &_stack {
+      text-align-last: left;
+      width: 100%;
+      &__title {
+      }
+
+      &__list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        background: rgba(150, 143, 143, 0.295);
+        padding: 10px;
+        border-radius: 20px;
+        li {
+          margin: 10px;
+          font-size: 20px;
+          height: 70px;
+          width: 70px;
+          display: flex;
+          justify-content: center;
+          align-items: flex-end;
+          color: #fff;
+          border-radius: 10px;
+          position: relative;
+          img {
+            width: 100%;
+            height: 100%;
+          }
+          span {
+            position: absolute;
+            bottom: -15px;
+          }
+          transition: 0.5s;
+          &:hover {
+            transform: scale(1.2);
+          }
+        }
+      }
+    }
+  }
+
+  &__avatar {
+    @media screen and (min-width: 990px) {
+      max-width: 400px;
+    }
+
+    &_image {
+      border: 10px solid rgb(233, 228, 228);
+      border-radius: 20px;
+    }
+  }
+  &__about {
+    background: rgba(102, 98, 98, 0.137);
+    font-size: 20px;
+    article {
+      padding: 10px;
+      text-align: left;
+      font-weight: 700;
+    }
+  }
+}
+</style>
